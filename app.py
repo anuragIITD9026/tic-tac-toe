2915560a9cc25f1a3e61a4e2c9ff4526325459fa
Anurag Dwivedi <anurag1710dwivedi@gmail.com>
2025-07-28 18:50:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import math
+
+app = Flask(__name__)
+CORS(app)
+
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)):
+        return True
+    if all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_full(board):
+    return all(cell != " " for row in board for cell in row)
+
+def minimax(board, depth, is_max):
+    if check_winner(board, 'O'):
+        return 1
+    if check_winner(board, 'X'):
+        return -1
+    if is_full(board):
+        return 0
+
+    if is_max:
+        best = -math.inf
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == " ":
+                    board[i][j] = 'O'
+                    best = max(best, minimax(board, depth + 1, False))
+                    board[i][j] = " "
+        return best
+    else:
+        best = math.inf
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == " ":
+                    board[i][j] = 'X'
+                    best = min(best, minimax(board, depth + 1, True))
+                    board[i][j] = " "
+        return best
+
+def best_move(board):
+    best_score = -math.inf
+    move = (-1, -1)
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == " ":
+                board[i][j] = 'O'
+                score = minimax(board, 0, False)
+                board[i][j] = " "
+                if score > best_score:
+                    best_score = score
+                    move = (i, j)
+    return move
+
+@app.route('/move', methods=['POST'])
+def move():
+    data = request.get_json()
+    board = data['board']
+    row, col = best_move(board)
+    return jsonify({'row': row, 'col': col})
+
+if __name__ == '__main__':
+    app.run(debug=True)
